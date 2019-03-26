aa4a33d7ddecd0da24e0e468caf1e02a14a5e044
June <719294349@qq.com>
2019-03-26 23:20:53 +0800

    modify

---
+++ README.md
@@ -0,0 +1,7 @@
+# TbMeishi
+Selenium+Chrome+PhantomJS爬取淘宝美食
+
+注：
+因为phantomjs不再支持selenium 最新的模块，所以如果要使用phantomjs就要安装旧版本selenium
+pip install selenium==3.8.0
+
---
+++ config.py
@@ -0,0 +1,13 @@
+MONGO_URL = 'localhost'
+
+MONGO_DB = 'toubao'
+
+MONGO_TABLE = 'product'
+
+SERVICE_ARGS = ['--load-images=[false]','--disk-cache=[true]']
+# SERVICE_ARGS = [
+#     '--proxy=113.106.249.42:80',
+#     '--proxy-type=socks5',
+# ]
+
+KEYWORD = '美食'
